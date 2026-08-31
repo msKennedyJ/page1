d10d2c784733a6cae9483b3d95907c1b7d08e558
msKennedyJ <jk@brillantmont.ch>
2026-08-31 15:05:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let grid = document.getElementById("grid");
+
 for (let i = 0; i < 400; i++) {
+
     let pixel = document.createElement("div");
+
     pixel.className = "pixel";
+
     pixel.onmousedown = function() {
         pixel.classList.add("on");
     };
-    pixel.onmouseover = function() {
+
+    pixel.onmouseover = function(event) {
         if (event.buttons === 1) {
             pixel.classList.add("on");
         }
     };
+
     grid.appendChild(pixel);
 }
 
