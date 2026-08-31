ee47507fff5c7b91934d01d43dde3cc2490ba26a
msKennedyJ <jk@brillantmont.ch>
2026-08-31 15:08:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let grid = document.getElementById("grid");
-
+let drawing = false;
+let drawingOn = true;
 for (let i = 0; i < 400; i++) {
-
     let pixel = document.createElement("div");
-
     pixel.className = "pixel";
-
     pixel.onmousedown = function() {
-        pixel.classList.add("on");
+        drawing = true;
+        drawingOn = !pixel.classList.contains("on");
+        pixel.classList.toggle("on");
     };
-
-    pixel.onmouseover = function(event) {
-        if (event.buttons === 1) {
-            pixel.classList.add("on");
+    pixel.onmouseover = function() {
+        if (drawing) {
+            if (drawingOn) {
+                pixel.classList.add("on");
+            } else {
+                pixel.classList.remove("on");
+            }
         }
     };
-
     grid.appendChild(pixel);
 }
+document.onmouseup = function() {
+    drawing = false;
+};
 
