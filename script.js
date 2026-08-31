7dc68bd83293a60403b32eda410cf0f4826d5bbe
msKennedyJ <jk@brillantmont.ch>
2026-08-31 14:59:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-// Find the grid
 let grid = document.getElementById("grid");
-
-// Make 400 pixels
 for (let i = 0; i < 400; i++) {
-
-    // Create a pixel
     let pixel = document.createElement("div");
-
-    // Give the pixel a class
     pixel.className = "pixel";
-
-    // When the pixel is clicked
     pixel.onclick = function() {
-
-        // Turn it on or off
         pixel.classList.toggle("on");
     };
-
-    // Add the pixel to the grid
     grid.appendChild(pixel);
 }
